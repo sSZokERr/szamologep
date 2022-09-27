bb976a5acd53d3d377b9e33e0b7986f7447b8bd3
szoker <MY_NAME@example.com>
2022-09-27 13:43:25 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './main.css'
 
+document.getElementById('one').addEventListener('click', addOne);
+document.getElementById('two').addEventListener('click', addTwo);
+document.getElementById('three').addEventListener('click', addThree);
+document.getElementById('four').addEventListener('click', addFour);
+document.getElementById('five').addEventListener('click', addFive);
+document.getElementById('six').addEventListener('click', addSix);
+document.getElementById('seven').addEventListener('click', addSeven);
+document.getElementById('eight').addEventListener('click', addEight);
+document.getElementById('nine').addEventListener('click', addNine);
+document.getElementById('zero').addEventListener('click', addZero);
+document.getElementById('ac').addEventListener('click', addAc);
+document.getElementById('plusmin').addEventListener('click', plusMin);
+document.getElementById('percent').addEventListener('click', percent);
+document.getElementById('divide').addEventListener('click', divide);
+document.getElementById('x').addEventListener('click', times);
+document.getElementById('minus').addEventListener('click', minus);
+document.getElementById('plus').addEventListener('click', plus);
+document.getElementById('comma').addEventListener('click', comma);
+document.getElementById('equal').addEventListener('click', equal);
